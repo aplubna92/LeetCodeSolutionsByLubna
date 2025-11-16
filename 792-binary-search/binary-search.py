7a04a3eb5d77938955456dabe5cf7f8a9e523307
Lubna A P <ap.lubna92@gmail.com>
2025-11-16 20:54:38 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (91.50%) - LeetSync

---
+++ 792-binary-search/binary-search.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        high = len(nums) - 1
+        low = 0
+        while low <= high:
+            mid = (low + high) //2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                low = mid+1
+            else:
+                high = mid-1
+        return -1
