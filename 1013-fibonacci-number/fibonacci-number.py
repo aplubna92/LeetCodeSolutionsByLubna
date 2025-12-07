c2f556b13494c1de1cb10a7a1de8c7b5e5e2c0e8
Lubna A P <ap.lubna92@gmail.com>
2025-12-07 21:39:32 +0530

    Time: 612 ms (30.06%) | Memory: 12.6 MB (9.93%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -4,4 +4,14 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        """ solution 1: using plain recursion . taking 12ms"""
         return n if n<=1 else self.fib(n-2)+self.fib(n-1)
+
+        """ taking 16ms time , still using recursion"""
+        # starting_grid = {0:0, 1:1}
+        # def fibo(n):
+        #     if n in starting_grid:
+        #         return starting_grid[n]
+        #     starting_grid[n] = fibo(n-2) + fibo(n-1)
+        #     return starting_grid[n]
+        # return fibo(n)
